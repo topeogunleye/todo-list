6c60a25805274492fb5c9a6eae8d29a5e2bff664
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-01 15:00:28 +0100

    Removed unused statusUpdates methods

--- src/statusUpdates.js
+++ src/statusUpdates.js
@@ -1,12 +1,4 @@
 export class statusUpdates {
-  static toggleTask(task) {
-    task.classList.toggle('completed');
-  }
-
-  static removeTask(task) {
-    task.remove();
-  }
-
   static updateStatus(task, status) {
     if (status === 'completed') {
       task.completed = true;
