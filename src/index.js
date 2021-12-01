594fc46736030f1a682528f5f8a34443c25b6ba6
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-01 11:51:12 +0100

    Add Local Storage Class and methods

--- src/index.js
+++ src/index.js
@@ -1,63 +1,53 @@
 import './style.scss';
+import { TASKS } from './tasks';
+import { statusUpdates } from './statusUpdates';
 
-// Array of todo-list tasks
+// Store Class: Handles localStorage
+class Store {
+  static getTasks() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [...TASKS]; // copy the array
+      console.log(tasks);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return tasks;
+  }
 
-const TASKS = [
-  {
-    description: 'Task 1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Task 2',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Task 4',
-    completed: false,
-    index: 4,
-  },
-  {
-    description: 'Task 5',
-    completed: false,
-    index: 5,
-  },
-];
+  static addTask(task) {
+    const tasks = Store.getTasks();
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 
-const todoList = document.querySelector('.tasks');
+  static changeTaskStatus(e) {
+    const TASKS = Store.getTasks();
 
-// Iterate through the array of todo-list tasks
-const renderTasks = (todoList) => {
-  TASKS.forEach((task) => {
-    // Create a new list item
-    const listItem = document.createElement('div');
-    // Add classname to the list item
-    listItem.classList.add('task');
+    const task = TASKS.find(
+      (t) => t.index === parseInt(e.target.id.split('-')[1])
+    );
+    if (e.target.checked) {
+      statusUpdates.updateStatus(task, 'completed');
+    } else {
+      statusUpdates.updateStatus(task, 'uncompleted');
+    }
 
-    // Add HTML to the list item
-    listItem.innerHTML = `
-    <input 
-      type="checkbox"
-      id="task-${task.index}"
-    />
-    <label for="task-${task.index}">
-      <span class="custom-checkbox"></span>
-      ${task.description}
-    </label>
-    <svg xmlns="http://www.w3.org/2000/svg" class="todo-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-    </svg>
-    `;
+    localStorage.setItem('tasks', JSON.stringify(TASKS));
+    console.log(TASKS);
+  }
 
-    // Add the list item to the todo-list
-    todoList.appendChild(listItem);
-  });
-};
+  static removeTask(task) {
+    const tasks = Store.getTasks();
+    tasks = tasks.filter((t) => t.id !== task.id);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+}
 
-document.addEventListener('DOMContentLoaded', renderTasks(todoList));
+
+
+// event listener for checkbox change
+document.querySelector('.tasks').addEventListener('change', (e) => {
+  Store.changeTaskStatus(e);
+});
