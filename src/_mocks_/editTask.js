7bc2b42ab64e854b2556b7c2e2cc2f489cb160d5
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-10 15:45:33 +0100

    Add Edit Task Mock

---
+++ src/_mocks_/editTask.js
@@ -0,0 +1,11 @@
+import localStorageMock from "./localStorageMock";
+
+// Edit a task from local storage
+const editTask = (task, newTask) => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  const index = tasks.findIndex(t => t.id === task.id);
+  tasks[index] = newTask;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+export default editTask;
